b3766d7a9c9278634e4615779fd4e6d9d7a30502
Parth Bhovad <parthbhovad710@gmail.com>
2025-03-22 23:35:57 +0530

    new component added

---
+++ frontend/src/components/user/UserAvatar.jsx
@@ -0,0 +1,6 @@
+import {Avatar} from '@primer/react'
+import React from 'react'
+
+export default function UserAvatar() {
+  return <Avatar size={48} src="https://avatars.githubusercontent.com/u/92997159?v=4" alt="Mona user avatar" />
+}
