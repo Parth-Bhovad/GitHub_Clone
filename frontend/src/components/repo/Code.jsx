233400464cf127f3708b5615736832e3a7c013cb
Parth Bhovad <parthbhovad710@gmail.com>
2025-03-22 23:37:54 +0530

    new component added

---
+++ frontend/src/components/repo/Code.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import "./Code.css"
+
+import Hr from "../utils/hr/Hr"
+import ActionMenu from "../utils/ActionMenu"
+import BranchName from '../utils/BranchName';
+import RepoTable from "../utils/RepoTable";
+import Blankslate from "../utils/BlankSlate";
+import RepoHeading from './RepoHeading';
+export default function Code() {
+    return (
+        <>
+            <div className="code-container">
+                <RepoHeading />
+                <Hr />
+                <div className="main">
+                    <div className="d-1">
+                        <span className='left-span'>
+                            <ActionMenu />
+                            <BranchName />
+                        </span>
+                        <span className='right-span'>
+                            <input type="text" />
+                            <ActionMenu />
+                            <ActionMenu />
+                        </span>
+                    </div>
+                    <div className="d-2">
+                        <RepoTable></RepoTable>
+                    </div>
+
+                    <Blankslate />
+                </div>
+            </div>
+        </>
+    );
+}
