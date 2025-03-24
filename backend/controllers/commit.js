a41f0df6f0448a5239c3a90cb0bd83edbcbe996d
Parth Bhovad <parthbhovad710@gmail.com>
2025-03-24 21:08:42 +0530

    changes as per changes in bucket structure to improve optimization

--- backend/controllers/commit.js
+++ backend/controllers/commit.js
@@ -5,23 +5,24 @@ const { v4: uuidv4 } = require('uuid');
 async function commitRepo(message) {
     console.log("Commit function called");
     const repoPath = path.resolve(process.cwd(), ".git");
-    const stagedPath = path.join(repoPath, "staing");
-    const commitPath = path.join(repoPath, "commits");
-
+    const stagedPath = path.join(repoPath, "staging");
+    const commitDir = path.join(repoPath, "commitDir");
+    const commitsPath = path.join(commitDir, commits);
 
     try {
         const commitId = uuidv4();
-        const commitDir = path.join(commitPath, commitId);
-        await fs.mkdir(commitDir, {recursive:true});
-        const files = await fs.readdir(stagedPath);
-        for (const file of files) {
-            await fs.copyFile(
-                path.join(stagedPath, file),
-                path.join(commitDir, file)
-            );
-        }
+        const commitJson = path.join(commitDir, "commitsJson");
+        // const commitJsonId = path.join(commitJson, commitId);
+        // await fs.mkdir(commitJsonId, {recursive:true});
+        // const files = await fs.readdir(stagedPath);
+        // for (const file of files) {
+        //     await fs.copyFile(
+        //         path.join(stagedPath, file),
+        //         path.join(commitDir, file)
+        //     );
+        // }
 
-        await fs.writeFile(path.join(commitDir, "commit.json"), JSON.stringify({message, date: new Date().toISOString()}));
+        await fs.writeFile(path.join(commitJson, `${commitId}.json`), JSON.stringify({msg:message, date: new Date().toISOString(), filePaths:"d", _id:commitId}));
         console.log(`Commit ${commitId} created with message : ${message}.`);
         
     } catch (error) {
