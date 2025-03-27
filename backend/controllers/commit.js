41683c5182b51d96e50c0a8e873daa75a3323725
Parth Bhovad <parthbhovad710@gmail.com>
2025-03-27 23:52:58 +0530

    recursion implimented

--- backend/controllers/commit.js
+++ backend/controllers/commit.js
@@ -1,28 +1,33 @@
 const fs = require("fs").promises;
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
+const recursion = require(".././experiment/recursion");
 
 async function commitRepo(message) {
     console.log("Commit function called");
     const repoPath = path.resolve(process.cwd(), ".git");
     const stagedPath = path.join(repoPath, "staging");
     const commitDir = path.join(repoPath, "commitDir");
-    const commitsPath = path.join(commitDir, commits);
 
     try {
+        let results = await recursion(stagedPath);
+        console.log(results);
         const commitId = uuidv4();
         const commitJson = path.join(commitDir, "commitsJson");
-        // const commitJsonId = path.join(commitJson, commitId);
-        // await fs.mkdir(commitJsonId, {recursive:true});
-        // const files = await fs.readdir(stagedPath);
-        // for (const file of files) {
-        //     await fs.copyFile(
-        //         path.join(stagedPath, file),
-        //         path.join(commitDir, file)
-        //     );
-        // }
+        
+        const relativePaths = results.map((result) => path.relative(stagedPath, result));
 
-        await fs.writeFile(path.join(commitJson, `${commitId}.json`), JSON.stringify({msg:message, date: new Date().toISOString(), filePaths:"d", _id:commitId}));
+        for (const result of results) {
+            let ans = result.replace(/\\staging\\/g, "\\commitDir\\commits\\");
+            let dirName = path.dirname(ans);
+            await fs.mkdir(dirName, {recursive:true});
+            await fs.copyFile(
+                result,
+                ans,
+            );
+        }
+
+        await fs.writeFile(path.join(commitJson, `${commitId}.json`), JSON.stringify({msg:message, date: new Date().toISOString(), filePaths:relativePaths, _id:commitId}));
         console.log(`Commit ${commitId} created with message : ${message}.`);
         
     } catch (error) {
