988406cb6dbd65019426413a77c866e9e36235ec
Parth Bhovad <parthbhovad710@gmail.com>
2025-03-27 23:59:46 +0530

    remote ref link added

--- frontend/src/components/repo/RepoGuide.jsx
+++ frontend/src/components/repo/RepoGuide.jsx
@@ -2,18 +2,28 @@ import React from "react";
 import RepoHeading from "./RepoHeading";
 import "./RepoGuide.css"
 const RepoGuide = () => {
-    <>
+    return(
+<>
         <RepoHeading/>
         <div className="setup">
             <h3>Quick setup-- create a new repository on the command line</h3>
             <div className="guide">
                 <span>node index.js init</span>
+                <br />
                 <span>node index.js add</span>
+                <br />
                 <span>node index.js commit {"<message>"}</span>
+                <br />
                 <span>node index.js push</span>
-                <span>node index.js remote add origin {"<link>"}</span>
+                <br />
+                <span>node index.js remote add origin {window.location.href}</span>
+                <br />
                 <span>node index.js push</span>
+                <br />
             </div>
         </div>
     </>
+    );
 }
+
+export default RepoGuide
