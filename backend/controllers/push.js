541dc78d328c0f4e32b13ebbdd6af0e44e396fce
Parth Bhovad <parthbhovad710@gmail.com>
2025-03-27 23:53:46 +0530

    recursion implimented

--- backend/controllers/push.js
+++ backend/controllers/push.js
@@ -2,43 +2,45 @@ const fs = require("fs").promises;
 const path = require("path");
 const { s3, S3_BUCKET } = require("../config/aws-config");
 const supabase = require("../config/supabaseConfig");
-const getUserIdFromToken = require("../utils/getUserIdFromToken");
-const express = require("express");
+const recursion = require("../experiment/recursion");
 
 async function pushRepo() {
     console.log("Push function called");
     const repoPath = path.resolve(process.cwd(), ".git");
-    const commitsPath = path.join(repoPath, "commits");
+    const commitDir = path.join(repoPath, "commitDir");
+    const commitPaths = path.join(commitDir, "commits");
 
-    const configPath = path.resolve(process.cwd(),".git", "userConfig", "userConfig.json");
+    const remoteRefPath = path.join(repoPath, "refs", "remoteRef.json");
 
-    const userConfig = await fs.readFile(configPath, "utf8")
-    const config = JSON.parse(userConfig);
-    const userId = config.userId;
+    const ref = await fs.readFile(remoteRefPath);
+    const { remoteRef } = JSON.parse(ref);
+    const url = new URL(remoteRef);
+    pathSegments = url.pathname.split("/").filter(Boolean);
+
+    const startUrl = `${pathSegments[0]}/${pathSegments[1]}`;
     try {
-        const commitDirs = await fs.readdir(commitsPath);
-        for (const commitDir of commitDirs) {
-            const commitPath = path.join(commitsPath, commitDir);
-            const fileName = path.basename(commitPath);
-            const files = await fs.readdir(commitPath);
+        let results = await recursion(commitPaths);
+        const relativePaths = results.map((result) => path.relative(commitPaths, result));
+        let i = 0;
+        for (const result of results) {
+            const finalUrl = path.join(startUrl, relativePaths[i]);
+            console.log(finalUrl);
+            const fileContent = fs.readFile(result);
+            const { data, error } = await supabase
+                .storage
+                .from('.git')
+                .upload(finalUrl, fileContent, { upsert: true });
 
-            for (const file of files) {
-                const filePath = path.join(commitPath, file);
-                const fileContent = await fs.readFile(filePath);
-                const { data, error } = await supabase
-                    .storage
-                    .from('.git')
-                    .upload(`${userId}/${fileName}/${file}`, fileContent, { upsert: true });
-
-                    if (error) {
-                        console.error("Upload failed:", error.message);
-                    } else {
-                        console.log(`Uploaded: ${file}`);
-                    }
+            if (error) {
+                console.error("Upload failed:", error.message);
+            } else {
+                console.log(`Uploaded: ${result}`);
             }
-        }
-        console.log("all commits pushed to S3.");
 
+            i++;
+        }
+
+        console.log("all commits pushed to S3.");
     } catch (error) {
         console.error("Error pushing to S3:", error);
     }
