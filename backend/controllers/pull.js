23dcd6080688aea4d4c749b9b3259d4deedab32f
Parth Bhovad <parthbhovad710@gmail.com>
2025-03-29 19:10:48 +0530

    pull function implimented using recursion

--- backend/controllers/pull.js
+++ backend/controllers/pull.js
@@ -2,106 +2,56 @@ const fs = require("fs").promises;
 const path = require("path");
 const { s3, S3_BUCKET } = require("../config/aws-config");
 const supabase = require("../config/supabaseConfig");
+const mongoose = require("mongoose");
 
-const getCommitId = async () => {
-    const configPath = path.resolve(process.cwd(),".git", "userConfig", "userConfig.json");
-    const userConfig = await fs.readFile(configPath, "utf8")
-    const config = JSON.parse(userConfig);
-    const userId = config.userId;
-
-    const { data, error } = await supabase
-        .storage
-        .from('.git')
-        .list(`${userId}`, {
-            limit: 100,
-            offset: 0,
-            sortBy: { column: 'name', order: 'asc' },
-        });
-
-
-    if (error) {
-        console.error("Error fetching file list:", error.message);
-    } else {
-        console.log("Files in storage:", data);
-    }
-    const commitId = data.map((data) => data.name);
-    console.log(commitId);
-    return commitId;
-}
-
-const getFileName = async (commitIds) => {
-    const configPath = path.resolve(process.cwd(), "config", "userConfig.json");
-    const userConfig = await fs.readFile(configPath, "utf8")
-    const config = JSON.parse(userConfig);
-    const userId = config.userId;
-
-    let fileNameArray = [];
-
-    for (const commitId of commitIds) {
-        const { data, error } = await supabase
-            .storage
-            .from('.git')
-            .list(`${userId}/${commitId}`, {
-                limit: 100,
-                offset: 0,
-                sortBy: { column: 'name', order: 'asc' },
-            });
-
-
-        if (error) {
-            console.error("Error fetching file list:", error.message);
-        } else {
-            console.log("Files in storage:", data);
-        }
-
-        let fileName = data.map((data) => data.name);
-        fileNameArray.push(...fileName);
-        console.log(fileName);
-    }
-
-    console.log(fileNameArray);
-    return fileNameArray;
-}
+//importing models
+const repoFilePaths = require("../models/repoFilePathsModel");
 
 async function pullRepo() {
     console.log("pull function called");
     const repoPath = path.resolve(process.cwd(), ".git");
-    const commitsPath = path.join(repoPath, "commits");
 
-    const configPath = path.resolve(process.cwd(), "config", "userConfig.json");
+    const remoteRefPath = path.join(repoPath, "refs", "remoteRef.json");
 
-    const userConfig = await fs.readFile(configPath, "utf8")
-    const config = JSON.parse(userConfig);
-    const userId = config.userId;
+    const ref = await fs.readFile(remoteRefPath);
+    const { remoteRef } = JSON.parse(ref);
+    const url = new URL(remoteRef);
+    let pathSegments = url.pathname.split("/").filter(Boolean);
 
+    const rootPath = path.resolve(process.cwd(), "root");
     try {
-        let commitIds = await getCommitId();
-        let fileNames = await getFileName(commitIds);
+        const mongoUri = process.env.MONGODB_URI;
+        mongoose.connect(mongoUri)
+            .then(() => console.log("MongoDB connected"))
 
-        for (const commitId of commitIds) {
+        const existingDoc = await repoFilePaths.findOne({ reponame: pathSegments[1] });
+        const filePaths = existingDoc.bucketFilePaths;
 
-            //make new directory for storing locally
-            await fs.mkdir(path.join(commitsPath, commitId));
+        for (const filePath of filePaths) {
+            const { data, error } = await supabase
+                .storage
+                .from('.git')
+                .download(filePath)
 
-            for (const fileName of fileNames) {
-                let pulledFilePath = `${commitId}/${fileName}`;
-
-                const { data, error } = await supabase
-                    .storage
-                    .from('.git')
-                    .download(`${userId}/${pulledFilePath}`);
-
-                // Convert the downloaded data (Buffer) to a file
-                const arrayBuffer = await data.arrayBuffer();
-                const buffer = Buffer.from(arrayBuffer);
-
-                // Save the file locally
-                await fs.writeFile(path.join(commitsPath, pulledFilePath), buffer);
-                console.log(`File downloaded and saved to: ${path.join(commitsPath, pulledFilePath)}`);
+            if (error) {
+                console.error("Download failed:", error);
             }
+            // Convert the downloaded data (Buffer) to a file
+            const arrayBuffer = await data.arrayBuffer();
+            const buffer = Buffer.from(arrayBuffer);
+
+            const dirName = path.dirname(filePath);
+            await fs.mkdir(path.join(rootPath, dirName), { recursive: true });
+
+            // Save the file locally
+            await fs.writeFile(path.join(rootPath, filePath), buffer);
+            console.log(`File downloaded and saved to: ${path.join(rootPath, filePath)}`);
         }
 
+        await mongoose.connection.close();
+        console.log("🧹 MongoDB connection closed.");
         console.log("All commits pulled from s3");
+        process.exit(0);
     }
     catch (error) {
         console.error("Unable to pull: ", error);
