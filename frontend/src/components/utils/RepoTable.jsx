e375dbdb3d615b49404cea9e41353601c27ce7e5
Parth Bhovad <parthbhovad710@gmail.com>
2025-04-03 15:55:58 +0530

    dynamic ness added in table and links

--- frontend/src/components/utils/RepoTable.jsx
+++ frontend/src/components/utils/RepoTable.jsx
@@ -1,37 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
 import Avatar from '../user/UserAvatar';
 import "./RepoTable.css"
 import "../../assets/colors.css";
 import { Link } from "react-router-dom";
 
-export default function RepositoryTable() {
+export default function RepositoryTable({rows, reponame, username}) {
     return (
         <div className="table">
             <div className="top">
                 <Avatar />username
             </div>
-            <Link to={"/repo/show/code"}>
+            {Object.keys(rows).map((key) => (
+                <Link to={`/${username}/${reponame}/show`}>
                 <div className="row">
-                    <span>filename</span>
+                    <span>{key}</span>
                     <span>Recent Commit</span>
                     <span>Time</span>
                 </div>
             </Link>
-            <div className="row">
-                <span>filename</span>
-                <span>Recent Commit</span>
-                <span>Time</span>
-            </div>
-            <div className="row">
-                <span>filename</span>
-                <span>Recent Commit</span>
-                <span>Time</span>
-            </div>
-            <div className="row">
-                <span>filename</span>
-                <span>Recent Commit</span>
-                <span>Time</span>
-            </div>
+            ))}
         </div>
     );
 }
