f81f4b469c81bc614c9e787bd92f595cd087aa06
Parth Bhovad <parthbhovad710@gmail.com>
2025-05-05 20:20:26 +0530

    props added for dynamic userAvatar

--- frontend/src/components/user/UserAvatar.jsx
+++ frontend/src/components/user/UserAvatar.jsx
@@ -1,6 +1,6 @@
 import {Avatar} from '@primer/react'
 import React from 'react'
 
-export default function UserAvatar() {
-  return <Avatar size={48} src="https://avatars.githubusercontent.com/u/92997159?v=4" alt="Mona user avatar" />
+export default function UserAvatar({src}) {
+  return <Avatar size={20} src={`${src}`} alt="Mona user avatar" />
 }
