9ec6937b83d41e05ea0026fc9b0d9f9f181c37ed
Parth Bhovad <parthbhovad710@gmail.com>
2025-05-13 12:56:40 +0530

    main.jsx and App.jsx used properly

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,16 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './styles/main.css'
 import "./styles/base/colors.css"
 import App from './App.jsx'
-import { AuthProvider } from './features/user/context/authContext.jsx'
-import ProjectRoutes from './Routes.jsx'
-import { BrowserRouter as Router } from 'react-router-dom'
-import Navbar from './components/layout/Navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <Router>
-    <Navbar/>
-      <ProjectRoutes />
-    </Router>
-  </AuthProvider>,
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
