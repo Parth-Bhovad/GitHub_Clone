537fe9767aacdde8f0bc67faa32a8af70090ce34
Parth Bhovad <parthbhovad710@gmail.com>
2025-05-13 17:12:35 +0530

    prop error solved

--- frontend/src/components/common/UserAvatar.jsx
+++ frontend/src/components/common/UserAvatar.jsx
@@ -2,5 +2,5 @@ import {Avatar} from '@primer/react'
 import React from 'react'
 
 export default function UserAvatar({src}) {
-  return <Avatar size={20} src={`${src}`} alt="Mona user avatar" />
+  return <Avatar size={20} src={src} alt="Mona user avatar" />
 }
