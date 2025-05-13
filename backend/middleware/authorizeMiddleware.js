1bfe960c024e01402f4f0f5d693b8715d31b318d
Parth Bhovad <parthbhovad710@gmail.com>
2025-05-13 17:11:26 +0530

    log statements removed

--- backend/middleware/authorizeMiddleware.js
+++ backend/middleware/authorizeMiddleware.js
@@ -2,13 +2,10 @@ const jwt = require('jsonwebtoken');
 const dotenv = require("dotenv");
 dotenv.config();
 function isLoggedIn(req, res, next) {
-    console.log("isLoggedIn");
-    
     const token = req.cookies.token;
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-        console.log("DECODED:", decoded);
         req.user = decoded;
         next();
     } catch (err) {
@@ -19,13 +16,9 @@ function isLoggedIn(req, res, next) {
 
 
 function isOwner(req, res, next) {
-    console.log("isOwner");
-
     let { id, username } = req.params;
     if (id) {
         if ( req.user._id.toString() !== id ) {
-            console.log("You can't access another user's data. ID");
-            
             return res.status(401).json({ message: "You can't access another user's data." });
         }
     }
