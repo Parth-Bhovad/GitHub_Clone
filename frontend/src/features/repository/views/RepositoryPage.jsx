17cdeef18f2413f66b4e1e01513b0c366b5d4216
Parth Bhovad <parthbhovad710@gmail.com>
2025-05-17 12:52:02 +0530

    Auth for repo implemented in frontend

--- frontend/src/features/repository/views/RepositoryPage.jsx
+++ frontend/src/features/repository/views/RepositoryPage.jsx
@@ -1,9 +1,31 @@
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
+//importing custom hooks
+import useRepoAuth from '../hooks/useRepoAuth';
+
+//importing context
+import { useAuthContext } from "../../user/context/authContext"
+
+// importing components
 import Repository from "../components/repo/Repository"
 
 function RepositoryPage() {
-    return ( 
-        <Repository/>
-     );
+    const { repository } = useRepoAuth();
+    const { currentUser } = useAuthContext();
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (repository.visibility === false) {
+            if (repository.owner != currentUser) {
+                navigate("/");
+            }
+        }
+    }, [repository]);
+    return (
+        <Repository />
+    );
 }
 
 export default RepositoryPage;
