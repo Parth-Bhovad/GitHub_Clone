80197c4c3163c546a87e14a72d7d588909ffbab4
Parth Bhovad <parthbhovad710@gmail.com>
2025-05-17 12:45:46 +0530

    nested a tags removed to prevent hydration error

--- frontend/src/components/layout/UnderlineNav.jsx
+++ frontend/src/components/layout/UnderlineNav.jsx
@@ -1,18 +1,24 @@
 import { UnderlineNav } from '@primer/react'
 import { Link } from 'react-router-dom';
 
-function UnderlineNavBar({reponame, username}) {
+function UnderlineNavBar({ reponame, username }) {
     return (
         <UnderlineNav aria-label="Repository">
-            <UnderlineNav.Item>
-                <Link to={`/${username}/${reponame}/code`}>Code</Link>
+            <UnderlineNav.Item as={Link} to={`/${username}/${reponame}/code`}>
+                Code
             </UnderlineNav.Item>
-            <UnderlineNav.Item>Pull requests</UnderlineNav.Item>
             <UnderlineNav.Item>
-                <Link to={`/${username}/${reponame}/issue`}>Issue</Link>
+                Pull requests
             </UnderlineNav.Item>
-            <UnderlineNav.Item>Projects</UnderlineNav.Item>
-            <UnderlineNav.Item>Wiki</UnderlineNav.Item>
+            <UnderlineNav.Item as={Link} to={`/${username}/${reponame}/issue`}>
+                Issue
+            </UnderlineNav.Item>
+            <UnderlineNav.Item>
+                Projects
+            </UnderlineNav.Item>
+            <UnderlineNav.Item>
+                Wiki
+            </UnderlineNav.Item>
         </UnderlineNav>
     );
 }
